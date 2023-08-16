59cfcb2037db8d91df6018f5b86699bd1652ff4e
assemned <assemned1@gmail.com>
2023-08-16 16:18:05 +0100

    last

--- js/script.js
+++ js/script.js
@@ -60,3 +60,23 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true,
 });
+
+
+
+/*====================== contact form =========================*/
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyz6_aYiUc19k0tmpKxt7uinnEPmG6ZZwbgrPXQVx_WeigDIIzP-iyvy6CER1Ioiwz5LQ/exec'
+        const form = document.forms['submit-to-google-sheet']
+        const msg = document.getElementById('msg')
+      
+        form.addEventListener('submit', e => {
+          e.preventDefault()
+          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => {
+                msg.innerHTML = "Message sent successfully"
+                setTimeout(function(){
+                msg.innerHTML = ""
+                },5000)
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error.message))
+        })
